b9557b3599fed8a4d860b4de79dd3c535aeec804
romberto <korhoff@mail.ru>
2022-12-18 13:16:23 +0400

    human_count_gallery

--- excursions/urls.py
+++ excursions/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.ExcursionsView.as_view(), name='excursions'),
     path('<int:id>/', views.ExcursionItemView.as_view(), name='excursion_item'),
-    re_path(r'^(\w+)(?P<count_human>\w+)/', views.ExcursionItemView.as_view(), name='excursion_item'),
+    re_path(r'^(<excursions>\w+)/(?P<count_human>\w+)', views.ExcursionItemView.as_view(), name='excursion_item'),
     path(r'<int:id>/<int:price>', views.ExcursionCustomItem.as_view(), name='excursion_custom_item'),
     path('check_phone/', views.checkPhone, name='check_phone'),
     path('check_code/', views.checkCode, name='check_code'),
